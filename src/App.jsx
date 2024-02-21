db926e3b827d07d1eec953c09cb763e220682d98
mississippibukkaker <mktds13@outlook.jp>
2024-02-21 15:49:44 +0900

    下準備

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+import { ChildArea } from "./Components/ChildArea";
+
+export const App = () => {
+  console.log("--app--")
+  const [text, setText] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onChangeText = (event) => {
+    setText(event.target.value);
+  }
+  const onClickToggle = () => {
+    setIsVisible(!isVisible)
+  }
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText}/>
+      <br />
+      <br />
+      <button onClick={onClickToggle}>表示</button>
+      <ChildArea open = {isVisible} />
+    </div>
+  );
+};
