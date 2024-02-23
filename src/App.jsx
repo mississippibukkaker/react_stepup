33bcea6abf7b2d5f775b98b2e97166083c372bdd
mississippibukkaker <mktds13@outlook.jp>
2024-02-23 14:08:45 +0900

    css-modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
 
 export const App = () => {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 };
