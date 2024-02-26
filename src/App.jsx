1675512e98df442be92b19e7c8f9761277e4f2ac
mississippibukkaker <mktds13@outlook.jp>
2024-02-26 13:53:55 +0900

    基本的なページ遷移-リンクの設置

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {BrowserRouter, Link} from "react-router-dom";
+
 import "./App.css";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -5,10 +7,17 @@ import { Page2 } from "./Page2";
 
 export const App = () => {
   return (
+    <BrowserRouter>
     <div className="App">
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/page1">Page1</Link>
+      <br />
+      <Link to="/page2">Pagee2</Link>
       <Home />
       <Page1 />
       <Page2 />
     </div>
+    </BrowserRouter>
   );
 };
