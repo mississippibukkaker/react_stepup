b8f3c5471a9e8a9bb63124c1211d30e016a9cfb2
mississippibukkaker <mktds13@outlook.jp>
2024-02-26 10:24:34 +0900

    styledJSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
 
 export const App = () => {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 };
