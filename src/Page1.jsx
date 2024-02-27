e7afd53b7be2ee5586a83ee4641e6f924b5792b0
mississippibukkaker <mktds13@outlook.jp>
2024-02-27 09:55:03 +0900

    Linkでstate渡すページ遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()]; // stateで渡す用
+console.log(arr)
   return (
     <div>
       <h1>this is page1.</h1>
-      <Link to="/page1/detailA">DetailA</Link>
+      <Link to="/page1/detailA" state={arr}>DetailA</Link>
+      {/* react v18 × react-router v6 だとLinkにstate直接渡せないっぽい？？（ので一旦放置） */}
       <br />
       <Link to="/page1/detailB">DetailB</Link>
       <br />
+      <button>DetailA</button>
     </div>
   );
 };
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,10 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+
+  const location = useLocation();
+  console.log(location);
+
   return (
     <div>
       <h1>this is page1 detail A.</h1>
