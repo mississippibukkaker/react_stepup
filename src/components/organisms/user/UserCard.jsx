d37ef60e2475adcbc302a4c11991919aa6a86aa2
mississippibukkaker <mktds13@outlook.jp>
2024-02-27 14:44:26 +0900

    Organism作成(Card内のアイコンとユーザー名をコンポーネント化)

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,18 +1,13 @@
 import styled from "styled-components";
 import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
 
 export const UserCard = (props) => {
   const { user } = props;
 
   return (
     <Card>
-      <img
-        height={160}
-        width={160}
-        src={user.image}
-        alt="プロフィール"
-      />
-      <p>{user.name}</p>
+      <UserIconWithName image={user.image} name={user.name} />
       <SDl>
         <dt>メール</dt>
         <dd>{user.email}</dd>
@@ -36,5 +31,6 @@ const SDl = styled.dl`
   dd {
     padding-left: 32px;
     padding-bottom: 8px;
+    overflow-wrap: break-word;
   }
 `;
