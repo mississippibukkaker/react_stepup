68a9427b2841b6a4e498c060a0ec005ff9c6285d
mississippibukkaker <mktds13@outlook.jp>
2024-02-27 18:08:38 +0900

    Page作成(Usersの中身Card表示)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
 import "./Styles.css";
 import { Router } from "./router/Router";
 
-const user = {
-  name: "じゃけえ",
-  image: "https://source.unsplash.com/NEOXGVKTmcA",
-  email: "12345@example.com",
-  phone: "090-1111-1111",
-  company: {
-    name: "テスト株式会社",
-  },
-  website: "https://google.com",
-};
-
 export const App = () => {
   return (
     <Router />
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `じゃけえ-${val}`,
+    image: "https://source.unsplash.com/NEOXGVKTmcA",
+    email: "12345@example.com",
+    phone: "090-1111-1111",
+    company: {
+      name: "テスト株式会社",
+    },
+    website: "https://google.com",
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+        ))}
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
 `;
