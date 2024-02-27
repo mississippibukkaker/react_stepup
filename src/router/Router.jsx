d5a3bb80c85abb27d3631479ef8c03c8bd0c634d
mississippibukkaker <mktds13@outlook.jp>
2024-02-27 10:35:31 +0900

    404ページを用意する

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "../Home";
 import { Page1Routes } from "./Page1Routes";
 import { Page2Routes } from "./Page2Routes";
+import { Page404 } from "../Page404";
 
 export const Router = () => {
   return (
@@ -9,6 +10,7 @@ export const Router = () => {
       <Route index element={<Home />} />
       <Route path="/page1/*" element={<Page1Routes />} />
       <Route path="/Page2/*" element={<Page2Routes />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 };
