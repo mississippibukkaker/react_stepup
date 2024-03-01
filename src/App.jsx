f899db0ca353de54e74e8f166fcbdd15013633bc
mississippibukkaker <mktds13@outlook.jp>
2024-03-01 23:36:46 +0900

    axiosでjsonplaceholder呼ぶ

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 export const App = () => {
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/user").then((res) => {
+      console.log(res.data);
+    }).catch((err)=>{console.log(err)});
+  };
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users?id=1").then((res) => {
+      console.log(res.data);
+    }).catch((err)=>{console.log(err)});
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
     </div>
   );
 };
